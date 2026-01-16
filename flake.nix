9a307b434ebdb74f320118ce1628b4301b60dce5
frsFallingSand <frsfallingsand@outlook.com>
2026-01-16 18:26:20 +0800

    refactor:move modules file location

--- flake.nix
+++ flake.nix
@@ -31,10 +31,10 @@
   let
     system = "x86_64-linux";
     # 自动扫描 modules 目录下的所有 .nix 文件
-    configDir = ./modules;
-    generatedModules = builtins.map (file: configDir + "/${file}") 
-      (builtins.filter (file: nixpkgs.lib.hasSuffix ".nix" file) 
-        (builtins.attrNames (builtins.readDir configDir)));
+    #configDir = ./modules;
+    #generatedModules = builtins.map (file: configDir + "/${file}") 
+    #  (builtins.filter (file: nixpkgs.lib.hasSuffix ".nix" file) 
+    #    (builtins.attrNames (builtins.readDir configDir)));
   in
   {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
@@ -43,7 +43,8 @@
       modules = [
         ./configuration.nix
         inputs.home-manager.nixosModules.default
-      ] ++ generatedModules; 
+      ]; 
+      #] ++ generatedModules; 
     };
   };
 }
