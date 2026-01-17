56380c43fdfdbfcb0b26b0beaadd6b5aae52a0a3
frsFallingSand <frsfallingsand@outlook.com>
2026-01-17 15:50:03 +0800

    feat:ii support test

--- flake.nix
+++ flake.nix
@@ -31,10 +31,22 @@
   let
     system = "x86_64-linux";
     # 自动扫描 modules 目录下的所有 .nix 文件
-    configDir = ./modules;
-    generatedModules = builtins.map (file: configDir + "/${file}") 
-      (builtins.filter (file: nixpkgs.lib.hasSuffix ".nix" file) 
-        (builtins.attrNames (builtins.readDir configDir)));
+    #configDir = ./modules;
+    #generatedModules = builtins.map (file: configDir + "/${file}") 
+    #  (builtins.filter (file: nixpkgs.lib.hasSuffix ".nix" file) 
+    #    (builtins.attrNames (builtins.readDir configDir)));
+
+    home_attrs = rec {
+      username = import ./username.nix;
+      homeDirectory = "/home/${username}";
+      # Do not edit stateVersion value, see https://github.com/nix-community/home-manager/issues/5794
+      stateVersion = "25.05";
+    };
+    lib = nixpkgs.lib;
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+
   in
   {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
@@ -43,7 +55,20 @@
       modules = [
         ./configuration.nix
         inputs.home-manager.nixosModules.default
-      ] ++ generatedModules; 
+      #] ++ generatedModules; 
+      ]; 
+    };
+
+    homeConfigurations = {
+      illogical_impulse = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = { inherit home_attrs 
+        #nixgl
+        quickshell; };
+        modules = [ 
+          ./home.nix
+        ];
+      };
     };
   };
 }
