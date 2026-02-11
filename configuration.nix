318340c55cc82a3dec74e4e0f4a1298ad0cf974a
frsFallingSand <frsfallingsand@outlook.com>
2026-02-11 09:06:47 +0800

    test: env

--- configuration.nix
+++ configuration.nix
@@ -446,7 +446,7 @@
     ];
     # From End4's hyprland dotfiles
     ELECTRON_OZONE_PLATFORM_HINT = "auto";
-    QT_QPA_PLATFORM = "wayland";
+    QT_QPA_PLATFORM = "wayland;xcb";
     QT_QPA_PLATFORMTHEME = "kde";
     XDG_MENU_PREFIX = "plasma-";
     TERMINAL = "kitty -1";
