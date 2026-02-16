d666038576f4cb3e7a5ab27fedac585ebec6ea3b
frsFallingSand <frsfallingsand@outlook.com>
2026-02-16 17:21:38 +0800

    feat: use my own nvimdots (forked and modified from the origin)

--- flake.nix
+++ flake.nix
@@ -4,7 +4,8 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    nvimdots.url = "github:ayamir/nvimdots";
+    # nvimdots.url = "github:ayamir/nvimdots";
+    nvimdots.url = "github:frsfallingsand/nvimdots";
 
     home-manager = {
       url = "github:nix-community/home-manager";
