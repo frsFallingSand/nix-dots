962947654661d667e988bfc6a3cc2c2d3052df12
frsFallingSand <frsfallingsand@outlook.com>
2026-04-30 17:58:57 +0800

    feat(fhs): enable fhs

--- configuration.nix
+++ configuration.nix
@@ -472,6 +472,34 @@
       cups-pdf-to-pdf
       neovim
       material-symbols
+      (
+        let
+          base = pkgs.appimageTools.defaultFhsEnvArgs;
+        in
+        pkgs.buildFHSEnv (
+          base
+          // {
+            name = "fhs";
+            targetPkgs =
+              pkgs:
+              # pkgs.buildFHSEnv provides only a minimal FHS environment,
+              # lacking many basic packages needed by most software.
+              # Therefore, we need to add them manually.
+              #
+              # pkgs.appimageTools provides basic packages required by most software.
+              (base.targetPkgs pkgs)
+              ++ (with pkgs; [
+                pkg-config
+                ncurses
+                zenity
+                # Feel free to add more packages here if needed.
+              ]);
+            profile = "export FHS=1";
+            runScript = "bash";
+            extraOutputsToInstall = [ "dev" ];
+          }
+        )
+      )
     ]
     # ++ [ nvim ];
     ++ [ lutr ];
