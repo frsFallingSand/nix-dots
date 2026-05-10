f1e6fb544af671c0cec7b06d1a18e4418f3ed954
frsFallingSand <frsfallingsand@outlook.com>
2026-05-10 14:09:47 +0800

    feat(user/fgsd): add to trusted-users group

--- configuration.nix
+++ configuration.nix
@@ -213,6 +213,7 @@
       "kvm"
       "docker"
       "dialout"
+      "trusted-users"
     ]; # Enable ‘sudo’ for the user.
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAEfAfx9NtJjJ+5aRopDw/1WZwXPPHM8SflPIweRNPWW frsfallingsand@outlook.com"
