80f218e5f366f95161a0e03eca9cd37d090ce2e5
frsFallingSand <frsfallingsand@outlook.com>
2026-06-12 16:16:54 +0800

    fix(nix)!: add electron to permittedInsecurePackages

--- configuration.nix
+++ configuration.nix
@@ -23,6 +23,10 @@
     dmsNixOSModule
   ];
 
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-39.8.10"
+  ];
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
